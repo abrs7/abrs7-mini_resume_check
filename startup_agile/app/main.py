17f8915085063c731d7c6badf50fffaef3057f38
abrahamasrat791@gmail.com <abrishsambong@gmail.com>
2025-04-20 23:49:40 +0300

    Added photo's base64 inot db

--- startup_agile/app/main.py
+++ startup_agile/app/main.py
@@ -1,3 +1,11 @@
+from fastapi import HTTPException, APIRouter
+from app.models.entities import ResumeMatch
+from app.models.database import SessionLocal
+from app.services.ranker import rank_resume
+from app.services.photo_check import check_photo
+from app.models.schemas import RankRequest, RankResponse
+from sqlalchemy.engine import Engine
+import traceback
 from fastapi import FastAPI
 from app.routes import router as rank_router
 
@@ -11,53 +19,36 @@ for route in app.routes:
     print("  ", route.path)
 
 
-
-
-
-from fastapi import HTTPException, APIRouter
-from app.models.entities import ResumeMatch
-from app.models.database import SessionLocal
-from app.services.ranker import rank_resume
-from app.services.photo_check import check_photo
-from app.models.schemas import RankRequest, RankResponse
-from sqlalchemy.engine import Engine
-import traceback
-
 @app.get("/test")
 def test():
-    print("🚨 /test endpoint hit", flush=True)
+    print("/test endpoint hit", flush=True)
     return {"message": "Router is working"}
-
 
 @app.post("/rank", response_model=RankResponse)
 async def rank(request: RankRequest):
     print("🧠 rank() called")
-
-    print("###############")
     try:
-        # Start DB session
         with SessionLocal() as db:
-            # DEBUG: Print DB URL
             engine = db.get_bind()
             if isinstance(engine, Engine):
                 print("🚀 Using DB:", engine.url)
 
-            # Run ranker logic
             result = rank_resume(request.resume, request.job_description)
 
+            photo_check_result = None
             if request.photo:
-                photo_result = check_photo(request.photo)
-                result.update(photo_result)
+                photo_check_result = check_photo(request.photo)
+                result.update(photo_check_result)
             else:
                 result["photo_check"] = None
 
-            # Save to DB
             record = ResumeMatch(
                 resume=request.resume,
                 job_description=request.job_description,
                 score=result["score"],
                 feedback=result["feedback"],
-                photo_check=result["photo_check"]
+                photo_check=result["photo_check"],
+                image_base64=request.photo
             )
             db.add(record)
             db.commit()
